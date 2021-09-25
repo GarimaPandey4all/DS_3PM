3c170d3458bf5d2cb6f9a33ab57306a0cde8f60f
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-09-25 12:04:23 +0530

    Add files via upload

--- CircularLinkedList/main.c
+++ CircularLinkedList/main.c
@@ -77,8 +77,132 @@ void insertAtEnd()
     }
 }
 
+void viewList()
+{
+    struct node *t;
+
+    if(last == NULL)
+    {
+        printf("List is Empty");
+    }
+    else {
+        t = last->link;
+
+        do {
+            printf("%d  ", t->info);
+            t = t->link;
+        }while(t != last->link);
+    }
+}
+
+void deleteNode()
+{
+    struct node *t, *ptr;
+    int search;
+
+    if(last == NULL)
+    {
+        printf("List is Empty");
+        return;
+    }
+    printf("Enter any value to be search:");
+    scanf("%d", &search);
+
+    //if only one node exist in a list
+    if(last == last->link && last->info == search)
+    {
+        t = last;
+        last = NULL;
+        free(t);
+        return;
+    }
+    //if only two nodes exist in a list
+    if(last->link->info == search)
+    {
+        t = last->link;
+        last->link = t->link;
+        free(t);
+        return;
+    }
+
+    //if we have more than two nodes in a list
+    ptr = last->link;
+
+    while(ptr->link != last)
+    {
+        if(ptr->link->info == search)
+        {
+            t = ptr->link;
+            ptr->link = t->link;
+            free(t);
+            return;
+        }
+
+        ptr = ptr->link;
+    }
+
+    //if we have more than two nodes in a list but we want to delete last node of a list
+    if(last->info == search)
+    {
+        t = last;
+        ptr->link = last->link;
+        last = ptr;
+        free(t);
+        return;
+    }
+
+    printf("Search is not found");
+    return;
+
+}
+
 int main()
 {
+    int choice;
+
+    while(1) // infinite loop
+    {
+        printf("\npress 1. Insert At Empty\n");
+        printf("\npress 2. Insert At Start\n");
+        printf("\npress 3. Insert At End\n");
+        printf("\npress 4. Delete\n");
+        printf("\npress 5. View\n");
+        printf("\npress 6. Exit\n");
+
+        printf("\n\nEnter your choice:");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+        case 1:
+            insertAtEmpty();
+            break;
+
+        case 2:
+            insertAtStart();
+            break;
+
+        case 3:
+            insertAtEnd();
+            break;
+
+        case 4:
+            deleteNode();
+            break;
+
+        case 5:
+            viewList();
+            break;
+
+        case 6:
+            exit(0);
+
+        default:
+            printf("Invalid Choice");
+
+        }
+    }
+
 
     return 0;
 }
